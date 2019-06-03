9c308283ee7f115e656a2d3a956363c640961546
Ilya <ilf@bk.ru>
2019-06-03 16:58:43 +0300

    Rotate img and save

--- render/src/pages/TimeSheet.vue
+++ render/src/pages/TimeSheet.vue
@@ -1,46 +1,58 @@
 <template>
-    <v-container>
-        <v-layout align-center justify-center>
-            <v-flex xs9>
-                <v-card>
-                    <v-card-text>
-                        <web-cam
-                                ref="webcam"
-                                :device-id="deviceId"
-                                width="100%"
-                                height="200px"
-                                @cameras="onCameras"
-                        ></web-cam>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
-        <v-layout class="mt-3">
-            <v-flex xs12>
-                <v-card>
-                    <v-card-title>
-                        Дата: {{ dateTime }}
-                    </v-card-title>
-                    <v-card-text>
-                        <v-select
-                                :items="$store.getters.users"
-                                label="Сотрудник"
-                                v-model="userSelected"
-                        ></v-select>
-                    </v-card-text>
-                    <v-card-actions v-show="userSelected">
-                        <v-spacer></v-spacer>
-                        <div v-show="inWork && !outWork">
-                            <v-btn flat color="warning" v-if="!inPause" @click="pauseWork">Пауза</v-btn>
-                            <v-btn flat color="info" v-else @click="unpauseWork" v-show="!unPause">Продолжить</v-btn>
-                        </div>
-                        <v-btn flat color="success" v-if="!inWork" @click="startWork">Приход</v-btn>
-                        <v-btn flat color="error" v-else @click="finishWork" v-show="!outWork">Уход</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container>
+    <v-layout align-center justify-center>
+      <v-flex xs9>
+        <v-card>
+          <v-card-text>
+            <web-cam
+                    :class="{'rotate--src-video': rotateImg}"
+                    ref="webcam"
+                    :device-id="deviceId"
+                    width="100%"
+                    height="200px"
+                    @cameras="onCameras"
+            ></web-cam>
+          </v-card-text>
+          <v-card-actions>
+            <v-layout class="justify-center">
+              <v-flex xs8>
+                <v-switch
+                        class="mt-0"
+                        v-model="rotateImg"
+                        label="Повернуть на 180°"
+                ></v-switch>
+              </v-flex>
+            </v-layout>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout class="mt-3">
+      <v-flex xs12>
+        <v-card>
+          <v-card-title class="pb-0">
+            Дата: {{ dateTime }}
+          </v-card-title>
+          <v-card-text class="pb-0">
+            <v-select
+                    :items="$store.getters.users"
+                    label="Сотрудник"
+                    v-model="userSelected"
+            ></v-select>
+          </v-card-text>
+          <v-card-actions v-show="userSelected" class="pt-0">
+            <v-spacer></v-spacer>
+            <div v-show="inWork && !outWork">
+              <v-btn flat color="warning" v-if="!inPause" @click="pauseWork">Пауза</v-btn>
+              <v-btn flat color="info" v-else @click="unpauseWork" v-show="!unPause">Продолжить</v-btn>
+            </div>
+            <v-btn flat color="success" v-if="!inWork" @click="startWork">Приход</v-btn>
+            <v-btn flat color="error" v-else @click="finishWork" v-show="!outWork">Уход</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -69,7 +81,8 @@
                 inWork: false,
                 outWork: false,
                 inPause: false,
-                unPause: false
+                unPause: false,
+                rotateImg: false
             }
         },
         watch: {
@@ -116,7 +129,7 @@
             async getTimeSheet() {
                 this.timeSheet = await this.$store.dispatch('getUserTimeSheet', {
                     user: this.userSelected,
-                    month:moment().format('MM'),
+                    month: moment().format('MM'),
                     year: moment().format('YYYY')
                 });
             },
@@ -126,7 +139,7 @@
             setTime() {
                 this.dateTime = moment().format('DD.MM.YYYY HH:mm:ss');
             },
-            startWork() {
+            async startWork() {
                 if (!this.timeSheet) {
                     this.timeSheet = {};
                 }
@@ -137,7 +150,7 @@
 
                 Object.assign(this.timeSheet[moment().format('DD')], {
                     startTime: +moment(),
-                    startFoto: this.$refs.webcam.capture(),
+                    startFoto: this.rotateImg ? await this.rotateImgFn(this.$refs.webcam.capture()) : this.$refs.webcam.capture(),
                     stopTime: 0,
                     pauseTime: 0,
                     unpauseTime: 0,
@@ -150,26 +163,26 @@
             async finishWork() {
                 Object.assign(this.timeSheet[moment().format('DD')], {
                     stopTime: +moment(),
-                    stopFoto: this.$refs.webcam.capture()
+                    stopFoto: this.rotateImg ? await this.rotateImgFn(this.$refs.webcam.capture()) : this.$refs.webcam.capture(),
                 });
 
                 this.saveTimeSheet('stop');
                 this.userSelected = '';
             },
-            pauseWork() {
+            async pauseWork() {
                 Object.assign(this.timeSheet[moment().format('DD')], {
                     pauseTime: +moment(),
-                    pauseFoto: this.$refs.webcam.capture(),
+                    pauseFoto: this.rotateImg ? await this.rotateImgFn(this.$refs.webcam.capture()) : this.$refs.webcam.capture(),
                     unpauseTime: 0,
                 });
 
                 this.saveTimeSheet('pause');
                 this.userSelected = '';
             },
-            unpauseWork() {
+            async unpauseWork() {
                 Object.assign(this.timeSheet[moment().format('DD')], {
                     unpauseTime: +moment(),
-                    unpauseFoto: this.$refs.webcam.capture()
+                    unpauseFoto: this.rotateImg ? await this.rotateImgFn(this.$refs.webcam.capture()) : this.$refs.webcam.capture()
                 });
 
                 this.saveTimeSheet('unpause');
@@ -181,8 +194,25 @@
                     timeSheet: this.timeSheet,
                     user: this.userSelected,
                     day: moment().format('DD'),
-                    month:moment().format('MM'),
+                    month: moment().format('MM'),
                     year: moment().format('YYYY')
+                });
+            },
+            rotateImgFn(imgBase) {
+                let image = new Image();
+                image.src = imgBase;
+                let canvas = document.createElement('canvas');
+                canvas.width = 640;
+                canvas.height = 480;
+                let ctx = canvas.getContext('2d');
+
+                return new Promise(resolve => {
+                    image.onload = () => {
+                        ctx.drawImage(image, 0, 0);
+                        ctx.rotate(180 * Math.PI / 180);
+                        ctx.drawImage(image, image.width * (-1), image.height * (-1));
+                        resolve(canvas.toDataURL('image/jpeg'));
+                    };
                 });
             }
         }
@@ -190,5 +220,7 @@
 </script>
 
 <style scoped>
-
+  .rotate--src-video {
+    transform: rotate(180deg);
+  }
 </style>
